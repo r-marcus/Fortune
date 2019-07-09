8ffc62b4bc8ebdb32a053928be7461f9e42aad0d
Rivka Marcus <rivkamarcus1234@gmail.com>
2019-07-09 15:47:56 -0400

    four

---
+++ fortune.py
@@ -0,0 +1,25 @@
+print ("I'm lost in the woods!")
+direct = input("Should I go right or left? Say r or l ")
+if direct==r:
+    print("Look at this nice beach!")
+    beach= input("Should I sunbathe or go swimming? Say sun or swim")
+    if beach==sun:
+            print("What a good choice! Thanks for helping me with my day!")
+    else:
+            print("I love swimming! Wow I'm in water!")
+else:
+    print("Wow I've never been inside a witch's house! ")
+    in=input("She's coming home! Should I hide or wait for her? Say wait or hide")
+    if in==wait:
+            print("I hope she's friendly!")
+            print("*a turtle wearing a wizard hat enters*")
+            print("Turtle Witch: Because you entered my house, I will cast a spell to kill you!")
+    else:
+            print("Where should I hide?")
+            print("I know! The cauldron")
+            print("I think this cauldron has poison in it!")
+            drown= input("Should I try to climb out? Say y/n")
+            if drown== y
+                    print("I can't do it!")
+                    continue
+            print("I'm drowning!")
